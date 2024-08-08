59adb9be68290857225b24c378efba5875751cd7
BenoitSafari <gregory.michalak.192@gmail.com>
2024-08-09 01:18:33 +0200

    release/1.0.2: FormFile extensions Core

---
+++ Safari.Net.Core/Extensions/FormFileUtilities/FormFileWriter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Safari.Net.Core.Messages;
+
+namespace Safari.Net.Core.Extensions.FormFileUtilities;
+
+public static class FormFileWriter
+{
+    public static Result TryWriteFile(this IFormFile form, string path)
+    {
+        var result = new Result<IFormFile>();
+        try
+        {
+            using var stream = new FileStream(path, FileMode.Create);
+            form.CopyTo(stream);
+        }
+        catch (Exception ex)
+        {
+            result.AddError(ex);
+        }
+
+        return result;
+    }
+
+    public static async Task<Result> TryWriteFileAsync(this IFormFile form, string path)
+    {
+        var result = new Result<IFormFile>();
+        try
+        {
+            await using var stream = new FileStream(path, FileMode.Create);
+            await form.CopyToAsync(stream);
+        }
+        catch (Exception ex)
+        {
+            result.AddError(ex);
+        }
+
+        return result;
+    }
+}
