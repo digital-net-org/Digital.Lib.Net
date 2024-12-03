630a0ffb39c51e9f15dddd770a10d59eb144d3f5
BenoitSafari <gregory.michalak.192@gmail.com>
2024-12-03 22:16:15 +0100

    refactor(script): Reworked build script

---
+++ .scripts/build.ps1
@@ -0,0 +1,43 @@
+﻿# Usage: .\publish.ps1 -k nugetKey
+
+param (
+    [string]$key
+)
+
+$projects = Get-ChildItem -Recurse -Filter *.csproj | Where-Object { $_.Name -notmatch "\.Test\.csproj$" }
+$deployTo = "https://api.nuget.org/v3/index.json"
+
+if (!$key) {
+    Write-Host "No API key provided, skipping publishing to ${deployTo}" -ForegroundColor Yellow
+}
+
+foreach ($project in $projects) {
+    $projectPath = $project.FullName
+    $projectName = [System.IO.Path]::GetFileNameWithoutExtension($projectPath)
+    [xml]$csproj = Get-Content $projectPath
+
+    $version = $csproj.Project.PropertyGroup.Version
+    $version = "${version}".Trim()
+    $packageFileName = "${projectName}.${version}.nupkg"
+    $packageSource = Join-Path -Path $project.DirectoryName "bin" "Release" $packageFileName
+    $packageDestination = Join-Path -Path $project.DirectoryName ".." ".build"
+    $packageDestinationFile = Join-Path -Path $packageDestination $packageFileName
+
+    Write-Host "Releasing ${projectName} ${version}" -ForegroundColor Green
+    Write-Host "Building..."
+    Invoke-Expression "dotnet build ${projectPath} -c Release" > $null 2>&1
+    Write-Host "Packing..."
+    Invoke-Expression "dotnet pack ${projectPath} -c Release" > $null 2>&1
+
+    if (Test-Path -Path $packageDestinationFile) {
+        Remove-Item -Path $packageDestinationFile
+    }
+    Move-Item -Path $packageSource -Destination $packageDestination
+
+    if ($key) {
+        Write-Host "Publishing..." -ForegroundColor Green
+        Invoke-Expression "dotnet nuget push ${packageDestinationFile} --api-key ${key} --source ${deployTo}"
+    }
+}
+
+Write-Host "Done" -ForegroundColor Green
