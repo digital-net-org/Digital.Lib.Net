12004772ebd36222d762be3eaff9a7069ccfa5d1
BenoitSafari <gregory.michalak.192@gmail.com>
2024-12-08 16:07:31 +0100

    feature(core): ConnectionString getters
    feature(core): ConnectionString getters

--- Digital.Net.Core/Application/ApplicationSettings.cs
+++ Digital.Net.Core/Application/ApplicationSettings.cs
@@ -1,4 +1,5 @@
 using Digital.Net.Core.Environment;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 
 namespace Digital.Net.Core.Application;
@@ -36,4 +37,37 @@ public static class ApplicationSettings
             .AddJsonFile("appsettings.json", true, true)
             .AddJsonFile($"appsettings.{AspNetEnv.Get}.json", true, true)
             .AddEnvironmentVariables();
+
+    /// <summary>
+    ///     Get the connection string from the provided WebApplicationBuilder.
+    /// </summary>
+    /// <param name="builder">The WebApplicationBuilder.</param>
+    /// <param name="target">The target connection string.</param>
+    /// <returns>The connection string.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when no connection string is found.</exception>
+    public static string GetConnectionString(this WebApplicationBuilder builder, string target = "Default") =>
+        builder.Configuration.GetConnectionString(target) ??
+        throw new InvalidOperationException(
+            "Connection string is not set in appsettings files or environment variables"
+        );
+
+    /// <summary>
+    ///     Try to get the connection string from the provided application arguments.
+    /// </summary>
+    /// <param name="args">The application arguments.</param>
+    /// <returns>The connection string or null.</returns>
+    public static string? GetConnectionString(string[]? args) =>
+        args is not null && args.Length > 0 ? args[0] : null;
+
+
+    /// <summary>
+    ///     Get the connection string from the provided project appsettings files.
+    /// </summary>
+    /// <param name="projectName">The name of the project.</param>
+    /// <param name="target">The target connection string.</param>
+    /// <returns>The connection string.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when no connection string is found.</exception>
+    public static string GetExternalConnectionString(string projectName, string target = "Default") =>
+        new ConfigurationBuilder().AddProjectSettings(projectName).Build().GetConnectionString(target)
+        ?? throw new InvalidOperationException($"No connection string found in {projectName} appsettings files");
 }
