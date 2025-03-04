a917d0c2155284f1eea1b3ccdedcb97cb24ac953
BenoitSafari <gregory.michalak.192@gmail.com>
2025-03-04 22:04:49 +0100

    refactor(entities): Removed user role

--- Digital.Lib.Net.Entities/Migrations/20250304203636_user_role_removal.Designer.cs
+++ Digital.Lib.Net.Entities/Migrations/20250304203636_user_role_removal.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Digital.Lib.Net.Entities.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Digital.Lib.Net.Entities.Migrations
 {
     [DbContext(typeof(DigitalContext))]
-    partial class DigitalContextModelSnapshot : ModelSnapshot
+    [Migration("20250304203636_user_role_removal")]
+    partial class user_role_removal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Digital.Lib.Net.Entities/Migrations/20250304203636_user_role_removal.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digital.Lib.Net.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class user_role_removal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                schema: "digital_core",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                schema: "digital_core",
+                table: "User",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
