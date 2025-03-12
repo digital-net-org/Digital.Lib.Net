1f924d8498d268a2d343d4586fd195a681192dec
BenoitSafari <gregory.michalak.192@gmail.com>
2025-03-12 01:49:43 +0100

    fix(defaults): Fixed regular expressions

--- Digital.Lib.Net.Core/Extensions/StringUtilities/RegularExpressions.cs
+++ Digital.Lib.Net.Core/Extensions/StringUtilities/RegularExpressions.cs
@@ -9,17 +9,18 @@ public static partial class RegularExpressions
 {
     [GeneratedRegex(UsernamePattern)]
     private static partial Regex UsernameRegex();
-    public const string UsernamePattern = @"^[a-zA-Z0-9.'@_-]{6,24}$";
+    public const string UsernamePattern = @"^[a-zA-Z0-9_-]{6,24}$";
     public static Regex Username => UsernameRegex();
 
     [GeneratedRegex(EmailPattern)]
     private static partial Regex EmailRegex();
-    public const string EmailPattern = @"^[^@]+@[^@]+\.[^@]{2,253}$";
+    public const string EmailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
     public static Regex Email => EmailRegex();
 
     [GeneratedRegex(PasswordPattern)]
     private static partial Regex PasswordRegex();
-    public const string PasswordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{12,128}$";
+    public const string PasswordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{12,128}$";
+
     public static Regex Password => PasswordRegex();
 
     [GeneratedRegex("(?<!^)([A-Z])")]
